29ea7044ae9ed151adc49517e6fb31b808f059d1
Furkan Ayaz <furkan.yz@outlook.com>
2024-12-08 10:49:49 +0300

    Initial Commit

---
+++ calculator-app/calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_appApp.swift
+//  calculator-app
+//
+//  Created by FURKAN AYAZ on 8.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
